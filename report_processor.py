9a653a10b0f91292a1f00f6b600000d718395a0d
SDEFTTH <sdeftthwl@gmail.com>
2026-08-19 22:34:48 +0530

    Remove date from report footer

--- report_processor.py
+++ report_processor.py
@@ -813,7 +813,7 @@ padding:16px;flex:1;min-width:420px;overflow:hidden;}}
     <h1>FTTH WARANGAL DASHBOARD</h1>
     <div class="sub">
       BBM Wise Provisioning Report of WGL OA as on {report_date:%d-%b-%Y}
-      &middot; Reported by VAMSHI KRISHNA ADEPU on {today_date:%d-%b-%Y}
+      &middot; Reported by VAMSHI KRISHNA ADEPU
     </div>
   </div>
 </div>
